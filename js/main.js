28b7e4a3f4e988de674f5798ff6c154be13431fc
Jerome Toole <jerometoole@gmail.com>
2015-07-29 15:25:12 +0100

    working image switcher!

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ $(function(){
     $('#webcam').photobooth().on("image",function( event, dataUrl ){
         $( "#gallery" ).append( '<img src="' + dataUrl + '" >');
     });
+
+    $('.hair-select').click( function () {
+        var hairurl = $(this).data("hair");
+        $(".tryer__image").css( "background-image", 'url(' + hairurl + ')'); //Theme2
+    })
 });
