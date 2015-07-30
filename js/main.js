8ccd345a86505bbb868edcd190c910151c511e8a
Jerome Toole <jerometoole@gmail.com>
2015-07-30 12:09:26 +0100

    better cam working.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,44 @@
+// Put event listeners into place
+window.addEventListener("DOMContentLoaded", function() {
+    // Grab elements, create settings, etc.
+    // var canvas = document.getElementById("canvas"),
+        // context = canvas.getContext("2d"),
+    var video = document.getElementById("video"),
+        videoObj = { "video": true },
+        errBack = function(error) {
+            console.log("Video capture error: ", error.code); 
+        };
+
+    // Put video listeners into place
+    if(navigator.getUserMedia) { // Standard
+        navigator.getUserMedia(videoObj, function(stream) {
+            video.src = stream;
+            video.play();
+        }, errBack);
+    } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+        navigator.webkitGetUserMedia(videoObj, function(stream){
+            video.src = window.webkitURL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    }
+    else if(navigator.mozGetUserMedia) { // Firefox-prefixed
+        navigator.mozGetUserMedia(videoObj, function(stream){
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    }
+    video.style.cssText = "-moz-transform: scale(-1, 1); \
+    -webkit-transform: scale(-1, 1); -o-transform: scale(-1, 1); \
+    transform: scale(-1, 1); filter: FlipH;";
+
+    // // Trigger photo take
+    // $("#snap").click( function() {
+    //     context.drawImage(video, 0, 0, 467, 350);
+    // });
+
+}, false);
+
 $(function(){
-    $(document).ready(function() {
-        $("#webcam").scriptcam();
-    });
 
     $('.hair-select').click( function () {
         event.preventDefault();
